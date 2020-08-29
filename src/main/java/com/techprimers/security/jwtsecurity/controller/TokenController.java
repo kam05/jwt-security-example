46640a2f2b85d8d3e0f7b660a504812241211617
kam05 <kamalsolanki05@gmail.com>
2020-08-29 14:51:14 +0530

    pushing jwt impl project

---
+++ src/main/java/com/techprimers/security/jwtsecurity/controller/TokenController.java
@@ -0,0 +1,28 @@
+package com.techprimers.security.jwtsecurity.controller;
+
+import com.techprimers.security.jwtsecurity.model.JwtUser;
+import com.techprimers.security.jwtsecurity.security.JwtGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+    @Autowired
+    private JwtGenerator jwtGenerator;
+
+//    public TokenController(JwtGenerator jwtGenerator) {
+//        this.jwtGenerator = jwtGenerator;
+//    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
